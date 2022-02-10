84192a41a8d4f04092dcc3ed20d6d0e4b4ebf8b2
Tomas Voboril <tvoboril@users.noreply.github.com>
2022-02-10 08:24:16 -0600

    Update Setup_Mac.sh

--- Setup_Mac.sh
+++ Setup_Mac.sh
@@ -10,45 +10,26 @@ cp *.ttf /Library/Fonts/.
 
 echo Install Homebrew, wget, cask, etc
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install jq
-brew install terraform
-brew install tmux
-brew install kubectx
+
 brew install watch
 brew install wget
-# brew install bash
 brew install dive
 brew install imagemagick
 brew install midnight-commander
-# brew install youtube-dl
-# brew install ffmpeg
 brew install tree
 brew install speedtest-cli
 brew install htop
-brew install awscli
-brew install aws-shell
-brew install cmake
 brew install iterm2
-#brew install brew-cask
-#brew install sshfs
-#brew cask search
-#brew cask uninstall app
 brew install neofetch
-
-
-# Core Functionality
-echo Install Core Apps
-brew install alfred
-brew install authy
-# brew cask install skype
-brew install vlc
-# brew cask install firefox
-brew install discord
+sudo gem install colorls
 brew install java
 brew install istat-menus
-#brew cask install --appdir="/Applications" evernote
-#brew cask install --appdir="/Applications" wunderlist
-#brew cask install --appdir="/Applications" clamxav
+
+#Biz Apps
+brew install discord
+brew install slack
+brew install clickup 
+brew install chrome
 
 # Development
 echo Install Dev Apps
@@ -57,35 +38,25 @@ brew install atom
 brew install sequel-pro
 brew install macvim
 brew install veracrypt
-brew install macfuse
-# brew cask install handbrake
-# brew cask install makeMKV
+brew install jq
+brew install terraform
+brew install tmux
+brew install kubectx
 brew install zenmap
 brew install wireshark
 brew install gpredict
 brew install go
-sudo gem install colorls
+brew install awscli
+brew install aws-shell
+brew install cmake
 
-# Google Slavery
-echo Install Google Apps
-# brew cask install --appdir="/Applications" google-chrome
-brew install  google-chrome
-# brew cask install  MkChromecast
 # Nice to have
 echo Install Some additional Apps
 brew install musescore
 brew install gimp
-# brew cask install parallels
-# brew cask install microsoft-office
-brew install adobe-creative-cloud
-open /usr/local/Caskroom/adobe-creative-cloud/latest
-echo Install Adobe Creative Cloud Now
-read -p "Press any key to continue... " -n1 -s
-echo  '\n'
-#brew install bash-completion@2
-#echo Now do bash completion stuff
-#read -p "Press any key to continue... " -n1 -s
-#echo  '\n'
+
+
+
 
 
 # cleanup
